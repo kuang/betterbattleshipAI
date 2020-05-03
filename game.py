359fe301fed374821f20ab86bc9469fa7285d8f7
Aram Baghdassarian <abagh0703@gmail.com>
2020-05-03 12:22:39 -0400

    added upate gameboard

--- game.py
+++ game.py
@@ -3,20 +3,22 @@ import random
 
 # dimension of board- can be changed later
 BOARD_SIZE = 10
+DEFAULT_CHAR = ' '
+HIT_CHAR = 'X'
 
 # inits game board
-game_board = np.zeros((BOARD_SIZE, BOARD_SIZE))
-AIRCRAFT_CARRIER_SIZE = 5  # 5
-BATTLESHIP_SIZE = 4  # 4
-DESTROYER_SIZE = 3  # 3
-SUBMARINE_SIZE = 3  # 2
-PATROL_BOAT_SIZE = 2  # 1
+game_board = [[DEFAULT_CHAR for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
+AIRCRAFT_CARRIER_SIZE = 5  # A
+BATTLESHIP_SIZE = 4  # B
+DESTROYER_SIZE = 3  # D
+SUBMARINE_SIZE = 3  # S
+PATROL_BOAT_SIZE = 2  # P
 
 # 1 = up, 2 = down, 3 = left, 4 = right
 SHIP_DIRECTIONS = [1, 2, 3, 4]
 
 """
-heads up on board stuff: numpy array goes by [ROW, COL]
+heads up on board stuff: array goes by [ROW, COL]
 if game_board[0,5] = 1:
 result is
 [[ 0.  0.  0.  0.  0.  1.  0.  0.  0.  0.]
@@ -40,11 +42,11 @@ def print_board_to_console():
 # set game board to a random positioning of ships
 # ships ARE allowed to touch
 def set_random_ships():
-    place_rand_ship(AIRCRAFT_CARRIER_SIZE, 5)
-    place_rand_ship(BATTLESHIP_SIZE, 4)
-    place_rand_ship(DESTROYER_SIZE, 3)
-    place_rand_ship(SUBMARINE_SIZE, 2)
-    place_rand_ship(PATROL_BOAT_SIZE, 1)
+    place_rand_ship(AIRCRAFT_CARRIER_SIZE, 'A')
+    place_rand_ship(BATTLESHIP_SIZE, 'B')
+    place_rand_ship(DESTROYER_SIZE, 'D')
+    place_rand_ship(SUBMARINE_SIZE, 'S')
+    place_rand_ship(PATROL_BOAT_SIZE, 'P')
 
 
 # place a ship of size ship_size, with marking ship_marking
@@ -55,7 +57,7 @@ def place_rand_ship(ship_size, ship_marking):
         row = random.randint(0, BOARD_SIZE-1)
         col = random.randint(0, BOARD_SIZE-1)
         # no ship at this coord yet
-        if game_board[row][col] == 0:
+        if game_board[row][col] == DEFAULT_CHAR:
             random.shuffle(SHIP_DIRECTIONS)  # choose random direction
             for direction in SHIP_DIRECTIONS:
                 if check_valid_ship_placement(ship_size, direction, row, col):
@@ -70,26 +72,25 @@ def check_valid_ship_placement(ship_size, direction, row, col):
         if row - ship_size + 1 < 0:
             return False
         for i in range(ship_size):
-            if game_board[row-i][col] != 0:
+            if game_board[row-i][col] != DEFAULT_CHAR:
                 return False
     elif direction == 2:  # down
         if row + ship_size > BOARD_SIZE:
             return False
         for i in range(ship_size):
-            # print(game_board[row][col])
-            if game_board[row+i][col] != 0:
+            if game_board[row+i][col] != DEFAULT_CHAR:
                 return False
     elif direction == 3:  # left
         if col - ship_size + 1 < 0:
             return False
         for i in range(ship_size):
-            if game_board[row][col-i] != 0:
+            if game_board[row][col-i] != DEFAULT_CHAR:
                 return False
     else:  # direction = 4, right
         if col + ship_size > BOARD_SIZE:
             return False
         for i in range(ship_size):
-            if game_board[row][col+i] != 0:
+            if game_board[row][col+i] != DEFAULT_CHAR:
                 return False
     return True
 
@@ -108,20 +109,38 @@ def place_ship(ship_size, ship_marking, direction, row, col):
         for i in range(ship_size):
             game_board[row][col+i] = ship_marking
 
+
 def has_game_ended():
     for row in game_board:
         for item in row:
-            if item != 0: return False
+            if item != DEFAULT_CHAR or item != HIT_CHAR:
+                return False
     return True
+
+
+# ship_hit should be the character of the type of ship
+def is_ship_sunk(ship_hit):
+    for row in game_board:
+        for item in row:
+            if item == ship_hit:
+                return False
+    return True
+
 
 # update game for attack on row, col
 # returns tuple (hit/miss, null/which ship sunk, game still on/game over)
 def update_gameboard(row, col):
     current = game_board[row][col]
-    if current == 0: # nothing there
-        return (0, 0, 0)
+    if current == DEFAULT_CHAR or current == HIT_CHAR:  # nothing there or they already hit it, miss
+        return 0, 0, 0
+    else:  # hit
+        ship_sunk = int(is_ship_sunk(current))
+        if ship_sunk:
+            game_ended = int(has_game_ended())
+            return 1, current, game_ended
+        return 1, 0, 0
 
 
 set_random_ships()
-# print_board_to_console()
-print(has_game_ended())
+print_board_to_console()
+# print(has_game_ended())
