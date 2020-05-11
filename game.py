119ab790065bc12740efb9ea1dd93ef6b85bbc18
Justin Kuang <zk65@cornell.edu>
2020-05-10 20:39:10 -0400

    changed print statement

--- game.py
+++ game.py
@@ -17,17 +17,17 @@ def start_game(model_func_next_move):
         num_turns += 1
         game_over = move_response[2]
         # print(move_response)
-    print('NUMBER OF MOVES: ' + str(num_turns))
+    # print('NUMBER OF MOVES: ' + str(num_turns))
     return num_turns
 
 
 def compute_avg_score(num_simulations, ai_next_move, ai_restart):
     total_score = 0
-    for i in range(num_simulations):
+    for _ in range(num_simulations):
         total_score += start_game(ai_next_move)
         ai_restart()
     return total_score/num_simulations
 
 # Every AI should have a "next_move" function that takes in move_response as well as a "restart" that resets all the
 # relevant variables
-print(compute_avg_score(100, random_ai.next_move, random_ai.restart))
+print("random AI's avg score: " + str(compute_avg_score(100, random_ai.next_move, random_ai.restart)))
